11eb85de131b76b32ad1e654aa6dd4b1ad2eeaac
Md. An Nahian Prince <annahian44@gmail.com>
2024-12-12 23:29:30 +0600

    Add files via upload

---
+++ MODULE3/PRACTICE3/myClass.dart
@@ -0,0 +1,14 @@
+class myClass {
+  var myName = ('Prince');
+  var alphabet = ['A', 'B', 'C', 'd'];
+
+  //CREATE a function
+  addTwoNumbers(int a, int b) {
+    print(a + b);
+  }
+
+//create another function
+  addThreenumbers(int a, int b, int c) {
+    print(a + b + c);
+  }
+}
