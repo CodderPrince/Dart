32afe43c303babc6fd63643330a0aab2bfa7d9d0
Md. An Nahian Prince <annahian44@gmail.com>
2024-12-12 23:29:59 +0600

    Add files via upload

---
+++ MODULE3/PRACTICE3/p3_1.dart
@@ -0,0 +1,12 @@
+//Module 3: batch 1 L 35 OOP concept
+
+//class
+class myClass {
+  var myName = ('Prince');
+}
+
+void main() {
+  print("hello world");
+  var obj = new myClass();
+  print(obj.myName);
+}
