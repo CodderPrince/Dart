175a63debc7ebf09d0f34f85353b1a560d278a50
Md. An Nahian Prince <annahian44@gmail.com>
2024-12-16 01:26:04 +0600

    Add files via upload

---
+++ MODULE4/PRACTICE4/p4_4.dart
@@ -0,0 +1,28 @@
+/*
+* Module 4: L-43 Dart Debugg
+* */
+
+/*
+* To find out my mistake
+* To test/check my code, that works well
+* To understand complex program flow
+* To work with complex action part by part
+* To improve my code
+* */
+
+void main() {
+  var x = 10;
+  var y = 20;
+
+  var add = x + y;
+  var sub = y - x;
+  var mul = x * y;
+  var div = x / y;
+  var mod = x % y;
+
+  print(add);
+  print(sub);
+  print(mul);
+  print(div);
+  print(mod);
+}
