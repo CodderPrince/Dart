f262a512f9bf404853c4001e72e511d3a9db8349
Md. An Nahian Prince <annahian44@gmail.com>
2024-12-15 21:54:55 +0600

    Add files via upload

---
+++ MODULE4/PRACTICE4/main.dart
@@ -0,0 +1,16 @@
+void main() {
+//create son object
+
+
+//create father object
+  //var fatObj = new Father();
+  //fatObj.baperTaka();
+
+  /*
+  Amader mul uddhesso holo jeno keu ar baper takar hisab na pai
+  se jonno amora baper class ke abstract kore dilam
+  er fole ar keu baper class ke direct access korte parbe na
+  akhon access korte gele take son er maddhomei korte hobe
+
+   */
+}
